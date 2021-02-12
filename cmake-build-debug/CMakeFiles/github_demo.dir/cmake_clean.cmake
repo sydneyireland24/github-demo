bd0c94dc0110bb584d41b2799ed3840966b7a005
Sydney Gibbs <sigibbs@smu.edu>
2021-02-12 13:25:50 -0600

    First Commit Of Porject

---
+++ cmake-build-debug/CMakeFiles/github_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/github_demo.dir/main.cpp.o"
+  "github_demo"
+  "github_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/github_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
